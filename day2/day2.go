74ad2adb2799b4f1868f6f47cbce703a4fdc9352
Fabjan Sukalia <fsukalia@gmail.com>
2022-12-09 13:15:44 +0100

    day2: solution for second part

--- day2/day2.go
+++ day2/day2.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	rock = iota
+	rock = iota + 1
 	paper
 	scissor
 )
@@ -53,19 +53,53 @@ func main() {
 		}
 
 		opponent := parseToken(tokens[0])
-		me := parseToken(tokens[1])
 
-		totalScore += me + 1
+		if false {
+			me := parseToken(tokens[1])
+			// part 1
+			totalScore += me
 
-		if me == opponent {
-			// draw
-			totalScore += 3
-		}
+			if me == opponent {
+				// draw
+				totalScore += 3
+			}
 
-		if ((me == rock) && (opponent == scissor)) ||
-			((me == paper) && (opponent == rock)) ||
-			((me == scissor) && (opponent == paper)) {
-			totalScore += 6
+			if ((me == rock) && (opponent == scissor)) ||
+				((me == paper) && (opponent == rock)) ||
+				((me == scissor) && (opponent == paper)) {
+				// win
+				totalScore += 6
+			}
+		} else {
+			// part 2
+			me := tokens[1]
+
+			if me == "X" {
+				// need to lose
+				switch opponent {
+				case rock:
+					totalScore += scissor
+				case paper:
+					totalScore += rock
+				case scissor:
+					totalScore += paper
+				}
+			} else if me == "Y" {
+				// draw
+				totalScore += opponent
+				totalScore += 3
+			} else if me == "Z" {
+				// need to win
+				totalScore += 6
+				switch opponent {
+				case rock:
+					totalScore += paper
+				case paper:
+					totalScore += scissor
+				case scissor:
+					totalScore += rock
+				}
+			}
 		}
 	}
 
